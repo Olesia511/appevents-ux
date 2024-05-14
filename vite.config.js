2b690428270d3f466d7e44e1bfc2c83fc5bdf483
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2024-05-14 11:23:58 +0300

    rename  workflow (deploy.yml)

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+import webfontDownload from "vite-plugin-webfont-dl";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve("appevents-ux", "index.html"),
-        // nested: resolve(__dirname, "nested/index.html"),
-      },
+  plugins: [
+    react(),
+    svgr(),
+    webfontDownload(["https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"]),
+  ],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      pages: "/src/pages",
+      assets: "/src/assets",
     },
   },
+  base: "/appevents-ux",
 });
